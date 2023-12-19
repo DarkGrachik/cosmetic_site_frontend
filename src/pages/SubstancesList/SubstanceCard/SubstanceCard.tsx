2099451f198797542a6772855df720403a498e14
DarkGrachik <melkij_grach@mail.ru>
2023-12-20 00:34:43 +0300

    first commit

---
+++ src/pages/SubstancesList/SubstanceCard/SubstanceCard.tsx
@@ -0,0 +1,39 @@
+import "./SubstanceCard.sass"
+import {Substance} from "../../../utils/types";
+import {Link} from "react-router-dom";
+import mockImage from "/src/assets/mock.png"
+
+const SubstanceCard = ({ substance, isMock }: {substance:Substance, isMock:boolean }) => {
+
+    const img = `http://127.0.0.1:8000/api/substances/${substance.id}/image/`
+
+    return (
+        <div className="card-wrapper">
+
+            <div className="preview">
+                <img src={isMock ? mockImage : img}  alt=""/>
+            </div>
+
+            <div className="card-content">
+
+                <div className="content-top">
+
+                    <h3 className="title">{substance.name}</h3>
+
+                </div>
+
+                <div className="content-bottom">
+
+                    <Link to={`/substances/${substance.id}`}>
+                        Подробнее
+                    </Link>
+
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default SubstanceCard;
